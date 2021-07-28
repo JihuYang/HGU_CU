72f93d32afbf3e1dce52a0093fe107c9ce16d53e
21900565@handong.edu <yijeeseul@gmail.com>
2021-07-28 20:04:05 +0900

    Feat : change header

--- src/main/java/com/walab/hgu/controller/HomeController.java
+++ src/main/java/com/walab/hgu/controller/HomeController.java
@@ -2,14 +2,20 @@ package com.walab.hgu.controller;
 
 import java.text.DateFormat;
 import java.util.Date;
+import java.util.List;
 import java.util.Locale;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.walab.hgu.service.ClubService;
+import com.walab.hgu.DTO.ClubDTO;
 
 /**
  * Handles requests for the application home page.
@@ -19,21 +25,32 @@ public class HomeController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 	
+	@Autowired
+	ClubService clubService;
 	/**
 	 * Simply selects the home view to render by returning its name.
 	 */
+//	@RequestMapping(value = "/", method = RequestMethod.GET)
+//	public String home(Locale locale, Model model) {
+//		logger.info("Welcome home! The client locale is {}.", locale);
+//		
+//		Date date = new Date();
+//		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+//		
+//		String formattedDate = dateFormat.format(date);
+//		
+//		model.addAttribute("serverTime", formattedDate );
+//		
+//		return "home";
+//	}
 	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
+	public ModelAndView header(ModelAndView mv) {
 		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+		List<ClubDTO> categoryNameList = clubService.getCategoryNameList();
+		mv.addObject("categoryNameList", categoryNameList);//인터셉터에서 넣어주기
+		mv.setViewName("home");
+		System.out.println(mv);
 		
-		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
-		
-		return "home";
+		return mv;
 	}
-	
 }
